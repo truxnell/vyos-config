5e61ef8e26859fde60521022653a0a43ceffe596
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-03-24 17:03:34 +1100

    fix: fix hw addr

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -2,11 +2,10 @@
 
 set interfaces ethernet eth0 address 'dhcp'
 set interfaces ethernet eth0 description 'WAN'
-set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9c'
+set interfaces ethernet eth0 hw-id '7c:2b:e1:13:f7:79'
 
 set interfaces ethernet eth1 address '10.8.10.249/24'
 set interfaces ethernet eth1 description 'LAN'
-set interfaces ethernet eth1 hw-id '7c:2b:e1:13:f7:79'
 set interfaces ethernet eth1 vif 10 address '10.8.10.1/24'
 set interfaces ethernet eth1 vif 10 description 'TRUSTED'
 set interfaces ethernet eth1 vif 20 address '10.8.20.1/24'
@@ -17,9 +16,10 @@ set interfaces ethernet eth1 vif 40 address '10.8.40.1/24'
 set interfaces ethernet eth1 vif 40 description 'VIDEO'
 set interfaces ethernet eth1 vif 50 address '10.8.50.1/24'
 set interfaces ethernet eth1 vif 50 description 'GUEST'
+set interfaces ethernet eth1 hw-id '7c:2b:e1:13:f7:7a'
 
-set interfaces ethernet eth2 hw-id '7c:2b:e1:13:f7:7a'
-set interfaces ethernet eth3 hw-id '7c:2b:e1:13:f7:7b'
+set interfaces ethernet eth2 hw-id '7c:2b:e1:13:f7:7b'
+set interfaces ethernet eth3 hw-id '7c:2b:e1:13:f7:7c'
 
 # set interfaces wireguard wg01 address '10.8.11.1/24'
 # set interfaces wireguard wg01 description 'WIREGUARD'
