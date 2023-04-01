20724df02bb6bc0181a35c7d7d1a03b4ec89a3e5
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-04-02 09:41:10 +1000

    fix: add nat template

---
+++ config-parts/templates/nat.sh
@@ -0,0 +1,97 @@
+#!/bin/vbash
+
+# LAN -> WAN masquerade
+set nat source rule 100 description 'LAN -> WAN'
+set nat source rule 100 destination address '0.0.0.0/0'
+set nat source rule 100 outbound-interface 'eth0'
+set nat source rule 100 translation address 'masquerade'
+
+# Forward HTTP(S) to ingress
+set nat destination rule 100 description 'HTTPS'
+set nat destination rule 100 destination port '443'
+set nat destination rule 100 inbound-interface 'eth0'
+set nat destination rule 100 protocol 'tcp'
+set nat destination rule 100 translation address '10.8.20.210'
+set nat destination rule 100 translation port '443'
+
+set nat destination rule 101 description 'HTTP'
+set nat destination rule 101 destination port '80'
+set nat destination rule 101 inbound-interface 'eth0'
+set nat destination rule 101 protocol 'tcp'
+set nat destination rule 101 translation address '10.8.20.210'
+set nat destination rule 101 translation port '80'
+
+#
+# # Force DNS
+# set nat destination rule 102 description 'Force DNS for IoT'
+# set nat destination rule 102 destination address '!10.5.0.4'
+# set nat destination rule 102 destination port '53'
+# set nat destination rule 102 inbound-interface 'eth1.30'
+# set nat destination rule 102 protocol 'tcp_udp'
+# set nat destination rule 102 translation address '10.5.0.4'
+# set nat destination rule 102 translation port '53'
+# #
+# set nat destination rule 103 description 'Force DNS for Video'
+# set nat destination rule 103 destination address '!10.5.0.4'
+# set nat destination rule 103 destination port '53'
+# set nat destination rule 103 inbound-interface 'eth1.50'
+# set nat destination rule 103 protocol 'tcp_udp'
+# set nat destination rule 103 translation address '10.5.0.4'
+# set nat destination rule 103 translation port '53'
+#
+# set nat destination rule 112 description 'Force DNS for Kids'
+# set nat destination rule 112 destination address '!10.5.0.4'
+# set nat destination rule 112 destination port '53'
+# set nat destination rule 112 inbound-interface 'eth1.40'
+# set nat destination rule 112 protocol 'tcp_udp'
+# set nat destination rule 112 translation address '10.5.0.4'
+# set nat destination rule 112 translation port '53'
+#
+# # Force NTP
+# set nat destination rule 105 description 'Force NTP for Servers'
+# set nat destination rule 105 destination address '!10.8.20.1'
+# set nat destination rule 105 destination port '123'
+# set nat destination rule 105 inbound-interface 'eth1.10'
+# set nat destination rule 105 protocol 'udp'
+# set nat destination rule 105 translation address '10.8.20.1'
+# set nat destination rule 105 translation port '123'
+#
+# set nat destination rule 106 description 'Force NTP for Trusted'
+# set nat destination rule 106 destination address '!10.8.10.1'
+# set nat destination rule 106 destination port '123'
+# set nat destination rule 106 inbound-interface 'eth1.20'
+# set nat destination rule 106 protocol 'udp'
+# set nat destination rule 106 translation address '10.8.10.1'
+# set nat destination rule 106 translation port '123'
+#
+# set nat destination rule 107 description 'Force NTP for IoT'
+# set nat destination rule 107 destination address '!10.8.30.1'
+# set nat destination rule 107 destination port '123'
+# set nat destination rule 107 inbound-interface 'eth1.40'
+# set nat destination rule 107 protocol 'udp'
+# set nat destination rule 107 translation address '10.8.30.1'
+# set nat destination rule 107 translation port '123'
+#
+# set nat destination rule 108 description 'Force NTP for Kids'
+# set nat destination rule 108 destination address '!10.8.40.1'
+# set nat destination rule 108 destination port '123'
+# set nat destination rule 108 inbound-interface 'eth1.40'
+# set nat destination rule 108 protocol 'udp'
+# set nat destination rule 108 translation address '10.8.40.1'
+# set nat destination rule 108 translation port '123'
+#
+# set nat destination rule 111 description 'Force NTP for Video'
+# set nat destination rule 111 destination address '!10.8.50.1'
+# set nat destination rule 111 destination port '123'
+# set nat destination rule 111 inbound-interface 'eth1.50'
+# set nat destination rule 111 protocol 'udp'
+# set nat destination rule 111 translation address '10.8.50.1'
+# set nat destination rule 111 translation port '123'
+
+# set nat destination rule 109 description 'Force NTP for Wireguard Trusted'
+# set nat destination rule 109 destination address '!10.0.11.1'
+# set nat destination rule 109 destination port '123'
+# set nat destination rule 109 inbound-interface 'wg01'
+# set nat destination rule 109 protocol 'udp'
+# set nat destination rule 109 translation address '10.0.11.1'
+# set nat destination rule 109 translation port '123'
