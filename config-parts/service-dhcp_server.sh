795bd25f2b0083f1dd7ecc9f52c48926930e0c23
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-04-06 08:56:37 +1000

    fix: try dhcp temp

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -3,6 +3,16 @@
 set service dhcp-server hostfile-update
 set service dhcp-server host-decl-name
 
+# Local VLAN
+set service dhcp-server shared-network-name TRUSTED authoritative
+set service dhcp-server shared-network-name TRUSTED ping-check
+set service dhcp-server shared-network-name TRUSTED subnet 10.8.0.0/24 default-router '10.8.0.1'
+set service dhcp-server shared-network-name TRUSTED subnet 10.8.0.0/24 domain-name 'natallan.com'
+set service dhcp-server shared-network-name TRUSTED subnet 10.8.0.0/24 lease '86400'
+set service dhcp-server shared-network-name TRUSTED subnet 10.8.0.0/24 name-server '10.5.0.4'
+set service dhcp-server shared-network-name TRUSTED subnet 10.8.0.0/24 range 0 start '10.8.0.200'
+set service dhcp-server shared-network-name TRUSTED subnet 10.8.0.0/24 range 0 stop '10.8.0.254'
+
 # Trusted VLAN
 set service dhcp-server shared-network-name TRUSTED authoritative
 set service dhcp-server shared-network-name TRUSTED ping-check
