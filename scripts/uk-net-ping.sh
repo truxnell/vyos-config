796ed5774bc7c9a3b4a215ad49a83bd3e2722432
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-04-08 08:58:36 +1000

    fix: add ping time

--- scripts/uk-net-ping.sh
+++ scripts/uk-net-ping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/bin/bash /config/scripts/uptime-kuma.sh M47JRLRJ7r
+/bin/bash /config/scripts/uptime-kuma.sh M47JRLRJ7r ping -c 1 1.1.1.1
 
 sleep 30
 
-/bin/bash /config/scripts/uptime-kuma.sh M47JRLRJ7r
+/bin/bash /config/scripts/uptime-kuma.sh M47JRLRJ7r ping -c 1 1.1.1.1
