e7a69ff99c88a5e44b07e444fe0d241cc2aacbae
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-04-20 20:08:08 +1000

    feat: add pikvm alias

--- config-parts/system-static_host_mapping.sh
+++ config-parts/system-static_host_mapping.sh
@@ -33,5 +33,6 @@ set system static-host-mapping host-name s3.trux.dev inet 10.8.20.11
 # Other hosts
 set system static-host-mapping host-name technocore.natallan.com inet 10.8.20.60
 set system static-host-mapping host-name technocore.natallan.com alias pikvm.trux.dev
+set system static-host-mapping host-name technocore.natallan.com alias pikvm.natallan.com
 
 set system static-host-mapping host-name prusa.natallan.com inet 10.8.30.120
