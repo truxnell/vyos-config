50ec9d0d03884342361bfac3cc690d906aae0019
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-06-13 19:45:14 +1000

    fix: whoops

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -1,19 +1,20 @@
 #!/bin/vbash
 
 # Forward HTTP(S) to ingress
-set nat destination rule 100 description 'HTTPS'
-set nat destination rule 100 destination port '443'
-set nat destination rule 100 inbound-interface 'eth0'
-set nat destination rule 100 protocol 'tcp'
-set nat destination rule 100 translation address '10.8.20.210'
-set nat destination rule 100 translation port '443'
-
-set nat destination rule 101 description 'HTTP'
-set nat destination rule 101 destination port '80'
-set nat destination rule 101 inbound-interface 'eth0'
-set nat destination rule 101 protocol 'tcp'
-set nat destination rule 101 translation address '10.8.20.210'
-set nat destination rule 101 translation port '80'
+# DEPRECATED due to cloudflare tunnel
+# set nat destination rule 100 description 'HTTPS'
+# set nat destination rule 100 destination port '443'
+# set nat destination rule 100 inbound-interface 'eth0'
+# set nat destination rule 100 protocol 'tcp'
+# set nat destination rule 100 translation address '10.8.20.210'
+# set nat destination rule 100 translation port '443'
+#
+# set nat destination rule 101 description 'HTTP'
+# set nat destination rule 101 destination port '80'
+# set nat destination rule 101 inbound-interface 'eth0'
+# set nat destination rule 101 protocol 'tcp'
+# set nat destination rule 101 translation address '10.8.20.210'
+# set nat destination rule 101 translation port '80'
 
 # Factorio
 set nat destination rule 112 description 'Factorio - Seablock'
