de231f1850f08aa5b0eb655c3c919e5c4cf787f3
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-07-13 15:46:48 +1000

    feat: remove cloudflare-ddns

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -4,17 +4,20 @@
 set container network services prefix '10.5.0.0/24'
 
 # cloudflare-ddns
-set container name cloudflare-ddns allow-host-networks
-set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
-set container name cloudflare-ddns environment DOMAINS value 'ipv4.trux.dev,games.trux.dev'
-set container name cloudflare-ddns environment IP6_PROVIDER value "none"
-set container name cloudflare-ddns environment TZ value 'Australia/Melbourne'
-set container name cloudflare-ddns environment PGID value "1000"
-set container name cloudflare-ddns environment PUID value "1000"
-set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.4'
-set container name cloudflare-ddns memory '0'
-set container name cloudflare-ddns restart 'on-failure'
-set container name cloudflare-ddns shared-memory '0'
+#
+# NO LONGER REQUIRED, I CAN HAZ STATIC IP NOW
+#
+# set container name cloudflare-ddns allow-host-networks
+# set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
+# set container name cloudflare-ddns environment DOMAINS value 'ipv4.trux.dev,games.trux.dev,irc.voltaicforge.com'
+# set container name cloudflare-ddns environment IP6_PROVIDER value "none"
+# set container name cloudflare-ddns environment TZ value 'Australia/Melbourne'
+# set container name cloudflare-ddns environment PGID value "1000"
+# set container name cloudflare-ddns environment PUID value "1000"
+# set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.4'
+# set container name cloudflare-ddns memory '0'
+# set container name cloudflare-ddns restart 'on-failure'
+# set container name cloudflare-ddns shared-memory '0'
 
 # coredns - main instance
 set container name coredns cap-add 'net-bind-service'
