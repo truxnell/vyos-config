c1ae7351e673f250d40edbc14eacccc53ba263fd
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-07-15 14:16:28 +1000

    feat: new apply

--- apply-config.sh
+++ apply-config.sh
@@ -1,16 +1,18 @@
 #!/bin/vbash
 # shellcheck shell=bash
 # shellcheck source=/dev/null
-dry_run=false
+dry_run=true
 
 if [ "$(id -g -n)" != 'vyattacfg' ]; then
-	exec sg vyattacfg -c "/bin/vbash $(readlink -f "$0") $@"
+	exec sg vyattacfg -c "/bin/vbash $(readlink -f "$0") $*"
 fi
 
-while getopts "d" options; do
+while getopts "c" options; do
 	case "${options}" in
-	d)
-		dry_run=true
+	# -c Commit changes - default is dry-run
+	c)
+		echo 'Will commit changes'
+		dry_run=false
 		;;
 	*)
 		echo 'error in command line parsing' >&2
@@ -33,6 +35,29 @@ if [ -f "/config/secrets.sops.env" ]; then
 	done
 fi
 
+# Apply environment to container (configuration) files
+restart_containers=""
+while IFS= read -r -d '' file; do
+	cfgfile="${file%.tmpl}"
+
+	shafile=$file.sha256
+	if ! test -e "$shafile"; then
+		echo "rebuild" >"$shafile"
+	fi
+
+	newsha=$(envsubst <"$file" | shasum -a 256 | awk '{print $1}')
+	oldsha=$(cat "$shafile")
+
+	if ! test "$newsha" == "$oldsha"; then
+		echo "Configuration changed for $file"
+		if ! "$dry_run"; then
+			envsubst <"$file" >"$cfgfile"
+			echo "$newsha" >"$shafile"
+			restart_containers="$restart_containers $(echo "$file" | awk -F / '{print $1}')"
+		fi
+	fi
+done < <(find containers -type f -name "*.tmpl" -print0)
+
 # Include VyOS specific functions and aliases
 source /opt/vyatta/etc/functions/script-template
 
@@ -52,11 +77,11 @@ if "$dry_run"; then
 	compare
 else
 	# Pull new container images
-	AVAILABLE_IMAGES=($(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }'))
-	CONFIG_IMAGES=($(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq))
+	mapfile -t AVAILABLE_IMAGES < <(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }')
+	mapfile -t CONFIG_IMAGES < <(sed -nr "s/set container name .* image '(.*)'/\1/p" /config/config-parts/* | uniq)
 
 	for image in "${CONFIG_IMAGES[@]}"; do
-		if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ " ${image} " ]]; then
+		if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ \ ${image}\  ]]; then
 			echo "Pulling image ${image}"
 			run add container image "${image}"
 		fi
@@ -64,8 +89,8 @@ else
 
 	# Commit and save
 	echo "Committing and saving config"
-	confirm
-	# save
+	commit
+	save
 
 	# Clean obsolete container images
 	IFS=$'\n' read -rd '' -a AVAILABLE_IMAGES <<<"$(run show container image | tail -n +2)"
@@ -75,15 +100,19 @@ else
 		image_id=$(echo "${image}" | awk '{ print $3 }')
 		image_name_tag="${image_name}:${image_tag}"
 
-		if [[ ! " ${CONFIG_IMAGES[*]} " =~ " ${image_name_tag} " ]]; then
+		if [[ ! " ${CONFIG_IMAGES[*]} " =~ \ ${image_name_tag}\  ]]; then
 			echo "Removing container ${image_name_tag}"
 			run delete container image "${image_id}"
 		fi
 	done
 
-	# Clean annoying overlay* folders
-	sudo find "/config" -name "overlay*" -type d -prune -exec rm -rf "{}" \;
-
+	# Restart containers
+	for container in $restart_containers; do
+		run restart container "$container"
+	done
 fi
+
+# Clean annoying overlay* folders
+sudo find "/config" -name "overlay*" -type d -prune -exec rm -rf "{}" \;
 
 exit
