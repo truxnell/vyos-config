d1af0288587d8ecb23e65de9222355cd1aaa3a08
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-09-18 19:03:58 +1000

    feat: re-add dynamic ip

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -4,20 +4,18 @@
 set container network services prefix '10.5.0.0/24'
 
 # cloudflare-ddns
-#
-# NO LONGER REQUIRED, I CAN HAZ STATIC IP NOW
-#
-# set container name cloudflare-ddns allow-host-networks
-# set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
-# set container name cloudflare-ddns environment DOMAINS value 'ipv4.trux.dev,games.trux.dev,irc.voltaicforge.com'
-# set container name cloudflare-ddns environment IP6_PROVIDER value "none"
-# set container name cloudflare-ddns environment TZ value 'Australia/Melbourne'
-# set container name cloudflare-ddns environment PGID value "1000"
-# set container name cloudflare-ddns environment PUID value "1000"
-# set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.4'
-# set container name cloudflare-ddns memory '0'
-# set container name cloudflare-ddns restart 'on-failure'
-# set container name cloudflare-ddns shared-memory '0'
+
+set container name cloudflare-ddns allow-host-networks
+set container name cloudflare-ddns environment CF_API_TOKEN value "${SECRET_CLOUDFLARE_DYNDNS_TOKEN}"
+set container name cloudflare-ddns environment DOMAINS value 'ipv4.trux.dev,mel.trux.dev'
+set container name cloudflare-ddns environment IP6_PROVIDER value "none"
+set container name cloudflare-ddns environment TZ value 'Australia/Melbourne'
+set container name cloudflare-ddns environment PGID value "1000"
+set container name cloudflare-ddns environment PUID value "1000"
+set container name cloudflare-ddns image 'docker.io/favonia/cloudflare-ddns:1.9.4'
+set container name cloudflare-ddns memory '0'
+set container name cloudflare-ddns restart 'on-failure'
+set container name cloudflare-ddns shared-memory '0'
 
 # wildcard certificate
 set container name lego-auto image 'ghcr.io/bjw-s/lego-auto:v0.1.0'
